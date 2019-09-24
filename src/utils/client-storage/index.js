4af398131e8ee85d62d2619bef9215cbfb180f66
denghuangwei <huangyoucp@gmail.com>
2019-09-24 09:34:00 +0800

    client storage

--- src/utils/client-storage/index.js
+++ src/utils/client-storage/index.js
@@ -25,25 +25,27 @@ export default {
         // hold the exception
       }
     }
-  
+
     storageObj = storageObj || cookieStorage
 
-    let storage = new Storage(storageObj, options)
-    const lsDescripter = {
-      configurable: false,
-      enumerable: false,
-      get () {
-        return storage
-      },
-      set () {
-        console.error('can not set the $ls')
+    const storage = new Storage(storageObj, options)
+    const getLsDescripter = (name) => {
+      return {
+        configurable: false,
+        enumerable: false,
+        get () {
+          return storage
+        },
+        set () {
+          console.error(`can not set the ${name}`)
+        }
       }
     }
 
-    Object.defineProperty(Vue.prototype, '$ls', lsDescripter)
+    Object.defineProperty(Vue.prototype, '$ls', getLsDescripter('$ls'))
 
-    Object.defineProperty(Vue, 'ls', lsDescripter)
+    Object.defineProperty(Vue, 'ls', getLsDescripter('ls'))
 
-    Object.defineProperty(window, 'VueLocalStorage', lsDescripter)
+    Object.defineProperty(window, 'VueLocalStorage', getLsDescripter('VueLocalStorage'))
   }
 }
