7510a2a4aa090e7d26735cef45a6ab833fb4052a
vm920316 <416915835@qq.com>
2019-09-24 17:17:13 +0800

    add find

--- src/router.js
+++ src/router.js
@@ -22,6 +22,14 @@ let router = new Router({
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
     },
     {
+      path: '/find',
+      name: 'find',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/Find.vue')
+    },
+    {
       path: '/login',
       name: 'login',
       // route level code-splitting
---
+++ src/views/Find.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="find">
+    <div class="head">
+      <div class="head-MusicRecognition"></div>
+      <input type="text" placeholder name="head-search" id="head-search" />
+      <div class="head-MusicPlaying"></div>
+    </div>
+
+    <div class="navbar"></div>
+
+    <div class="nav-items">
+      <div class="nav-item">
+        <div class="item-pho"></div>
+        <span class="item-name">每日推荐</span>
+      </div>
+      <div class="nav-item"></div>
+      <div class="nav-item"></div>
+      <div class="nav-item"></div>
+      <div class="nav-item"></div>
+    </div>
+
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+* {
+  padding: 0px;
+  margin: 0px;
+}
+
+.find {
+  width: 90%;
+  height: 100%;
+  text-align: center;
+  margin: 10px auto 0px;
+}
+
+.head {
+  width: 100%;
+  height: 40px;
+  display: flex;
+  margin-bottom: 10px;
+}
+
+.head-MusicRecognition,
+.head-MusicPlaying {
+  width: 40px;
+  height: 40px;
+  background-color: pink;
+  flex-shrink: 0;
+}
+
+#head-search {
+  height: 40px;
+  width: 100%;
+  border-radius: 20px;
+  background-color: rgb(240, 240, 240);
+  border: 0px solid red;
+  margin: 0px 10px;
+}
+
+.navbar {
+  width: 100%;
+  height: 150px;
+  background-color: pink;
+  border-radius: 10px;
+}
+
+.nav-items {
+  display: flex;
+  justify-content: space-between;
+  margin: 20px 0px;
+  padding-bottom: 20px;
+  border-bottom: 1px solid rgb(199, 199, 199);
+}
+.nav-items .nav-item {
+  width: 60px;
+  height: 100px;
+  background-color: pink;
+}
+
+.nav-items .nav-item .item-pho{
+    width: 50px;
+    height:50px;
+    border-radius: 25px;
+    background-color: green;
+}
+.nav-items .nav-item .item-name{font-size: 12px;}
+</style>
