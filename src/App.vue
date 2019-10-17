c72a19d2ae722522c59345921e2204944eb958e9
denghuang <huangyoucp@gmail.com>
2019-10-17 18:06:51 +0800

    transition

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,12 @@
     <template v-if="!openPlayDialog">
       <router-view />
     </template>
-    <play-dialog v-show="openPlayDialog"></play-dialog>
+    <transition
+      name="slide-fade"
+      :duration="2000"
+    >
+      <play-dialog v-show="openPlayDialog"></play-dialog>
+    </transition>
   </div>
 </template>
 
@@ -25,5 +30,18 @@ export default {
 * {
   margin: 0px;
   padding: 0px;
+}
+/* 可以设置不同的进入和离开动画 */
+/* 设置持续时间和动画函数 */
+.slide-fade-enter-active {
+  transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active for below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
 }
 </style>
